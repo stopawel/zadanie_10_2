46f9e313783efcad12d99e8a980740028ade508f
Paweł Stolecki <noigell@gmail.com>
2018-03-17 16:30:25 +0100

    zadanie-10-2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+
+	function changeSlide(){
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+	function moveFirstSlide(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+	setInterval(changeSlide, 3000);
+});
